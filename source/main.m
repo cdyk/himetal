8b1a0dd006612f4308c4ff9eae1ad453e60c28d1
Christopher Dyken <cdyk@users.noreply.github.com>
2021-12-23 22:35:10 +0100

    mouse and keyboard

--- source/main.m
+++ source/main.m
@@ -46,6 +46,7 @@
 
 @implementation MyMTKView
 
+NSPoint _mClickStart;
 MyRenderDelegate* _mRenderer;
 
 - (instancetype)initWithFrame:(CGRect)frameRect device:(nullable id<MTLDevice>)device {
@@ -56,31 +57,58 @@ MyRenderDelegate* _mRenderer;
     }
     return self;
 }
-- (void)mouseDown:(NSEvent *)event {
-    NSLog(@"Mousedown");
+
+- (void)dealloc {
+    [super dealloc];
+    [_mRenderer release];
 }
 
+-(BOOL)acceptsFirstResponder {
+    return YES;
+}
+
+- (void)mouseDown:(NSEvent *)event {
+    _mClickStart = [self convertPoint:[event locationInWindow] fromView:nil];
+}
+
+- (void)mouseDragged:(NSEvent *)event {
+    NSPoint p = [self convertPoint:[event locationInWindow] fromView:nil];
+    NSLog(@"Mousedrag delta %f %f", p.x - _mClickStart.x, p.y - _mClickStart.y);
+}
+
+- (void)keyDown:(NSEvent *)event {
+    NSString* c = [event charactersIgnoringModifiers];
+    if([c isEqual:@"q"]) {
+        [NSApp terminate:self];
+    }
+    else {
+        [super keyDown:event];
+    }
+}
 @end
+
 
 int main(int argc, char** argv)
 {
-    [NSApplication sharedApplication];
-    NSWindow* win = [[NSWindow alloc] initWithContentRect:NSMakeRect(10, 10, 500, 300)
-                                                styleMask:NSWindowStyleMaskTitled |
-                                                          NSWindowStyleMaskClosable |
-                                                          NSWindowStyleMaskTitled |
-                                                          NSWindowStyleMaskResizable |
-                                                          NSWindowStyleMaskMiniaturizable
-                                                  backing:NSBackingStoreBuffered
-                                                    defer:NO];
+    @autoreleasepool {
+        [NSApplication sharedApplication];
+        NSWindow* win = [[[NSWindow alloc] initWithContentRect:NSMakeRect(10, 10, 500, 300)
+                                                     styleMask:NSWindowStyleMaskTitled |
+                                                               NSWindowStyleMaskClosable |
+                                                               NSWindowStyleMaskTitled |
+                                                               NSWindowStyleMaskResizable |
+                                                               NSWindowStyleMaskMiniaturizable
+                                                       backing:NSBackingStoreBuffered
+                                                         defer:NO] autorelease];
 
-    id view = [[MyMTKView alloc] initWithFrame:win.contentLayoutRect
-                                        device:MTLCreateSystemDefaultDevice()];
-    [win setTitle:@"Hi Metal"];
-    [win setContentView:view];
-    [win setIsVisible:YES];
-    [win autorelease];
-    [win makeMainWindow];
-    [NSApp run];
+        id view = [[[MyMTKView alloc] initWithFrame:win.contentLayoutRect
+                                             device:MTLCreateSystemDefaultDevice()] autorelease];
+        [win setTitle:@"Hi Metal"];
+        [win setContentView:view];
+        [win setIsVisible:YES];
+        [win autorelease];
+        [win makeMainWindow];
+        [NSApp run];
+    }
 	return 0;
 }
